f13458f1f574fc52b44c5ffce0b9013fbce01482
Роман Резниченко <romanreznichenko@mail.ru>
2015-11-22 23:45:53 +0300

    domashka

--- jsdz/js/example.js
+++ jsdz/js/example.js
@@ -1,3 +1,25 @@
 /**
  * Created by romanreznicenko on 19.11.15.
  */
+
+    var age = window.prompt("Enter your age");
+
+    if (age > 0 && age < 10) {
+        console.log("You're kid");
+    }
+    else if (age > 10 && age < 18){
+        console.log("You're teenager");
+    }
+    else if (age > 18 && age <25){
+        console.log("You're young adult");
+    }
+    else if (age > 25 && age < 65){
+        console.log("You're adult");
+    }
+    else if (age > 65 && age < 130){
+        console.log("Your're renter");
+    }
+
+    var ageNew = 18;
+    var bool = (ageNew >= 18)?("ok"):("not-ok");
+    console.log(bool);
